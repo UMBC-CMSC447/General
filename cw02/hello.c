3052bf82730a127356906799fa775eac044b889e
Michael Wilson <mwilson3@umbc.edu>
2012-09-05 22:34:16 -0400

    Hello C programs.

---
+++ cw02/hello.c
@@ -0,0 +1,15 @@
+// This is a comment.  This is text that you can use to annotate your code.
+// It's very handy when trying to explain things that are unusual or difficult
+// to understand.
+
+#include <stdio.h> // This is an include.  You need this one (stdio.h) in order
+                   // to print out.  More on these later.
+
+// This is the "main" function.  Every C program needs to have a main.
+// Additionally, it needs to be defined in this way.
+int main()
+{
+    printf("Hello, World!\n"); // This will print "Hello, World!"
+
+    return 0; // This is the return value of main.  More on this later.
+}
