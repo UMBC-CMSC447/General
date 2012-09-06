15a4654339e75bfca157757b078a916f85e3f619
Michael Wilson <mwilson3@umbc.edu>
2012-09-05 22:34:52 -0400

    Classwork 2

---
+++ cw02/count_up.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("One\n");
+    printf("Two\n");
+    printf("Three\n");
+    printf("Four\n");
+    printf("Five\n");
+    printf("Six\n");
+    printf("Seven\n");
+    printf("Eight\n");
+    printf("Nine\n");
+    printf("Ten\n");
+
+    return 0;
+}
